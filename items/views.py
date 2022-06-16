f38e7a208f45906e0fd54bdba1c689ab04e02a3b
ArekFrel <arkadiuszfrel@gmail.com>
2022-06-16 20:21:26 +0200

    urls.py added

--- items/views.py
+++ items/views.py
@@ -1,12 +1,19 @@
-from django.views.generic import TemplateView, ListView, FormView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, FormView, DeleteView, UpdateView, DetailView
 
 from items.forms import CompanyModelForm
 from items.models import Company
 
 
-class CompanyTemplateView(TemplateView):
-    template_name = "items/companies.html"
-    extra_context = {"companies": Company.objects.all()}
+class CompanyDeleteView(DeleteView):
+    model = Company
+    template_name = "items/delete.html"
+    success_url = reverse_lazy("items_app:companies-template-view")
+
+
+class CompanyDetailView(DetailView):
+    model = Company
+    template_name = "items/my_company.html"
 
 
 class CompanyListView(ListView):
@@ -24,3 +31,14 @@ class CompanyModelFormView(FormView):
         result = super().form_valid(form)
         form.save()
         return result
+
+
+class CompanyTemplateView(TemplateView):
+    template_name = "items/companies.html"
+    extra_context = {"companies": Company.objects.all()}
+
+
+class CompanyUpdateView(UpdateView):
+    model = Company
+    fields = "__all__"
+    template_name = "form.html"
