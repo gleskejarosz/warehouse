272510073516b1c83e48be72543c9cf9ef014133
ArekFrel <arkadiuszfrel@gmail.com>
2022-06-16 20:27:42 +0200

    urls.py added, cleaned

--- items/urls.py
+++ items/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from . import views
-import items.views
 
 app_name = "items_app"
 
@@ -9,8 +8,7 @@ urlpatterns = [
     path("items-create-view/", views.ItemCreateView.as_view(), name="items-create-view"),
     path("items-detail-view/<pk>/", views.ItemDetailView.as_view(), name="items-detail-view"),
     path("items-update-view/<pk>/", views.ItemUpdateView.as_view(), name="items-update-view"),
-    path("items-delete-view/<pk>/", views.ItemDeleteView.as_view(),
-         name="items-delete-view"),
+    path("items-delete-view/<pk>/", views.ItemDeleteView.as_view(), name="items-delete-view"),
     path("items-list-view/", views.ItemListView.as_view(), name="items-list-view"),
     path('items/index/', views.index, name="index"),
     path('items/', views.items, name="items"),
