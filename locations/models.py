ab76d1e9d94b6d9328042e9d94bf54d93673ac2a
Tomek <cartelpix@gmail.com>
2022-07-03 09:48:44 +0200

    poprawki2 do modelu Location

--- locations/models.py
+++ locations/models.py
@@ -13,8 +13,7 @@ class LocationDetail(models.Model):
     warehouse = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="warehouse")
     loc_section = models.IntegerField(blank=True, null=True)
     loc_shelf = models.CharField(max_length=4, blank=True, null=True)
-    in_location = models.BooleanField(default=False)
-    out_location = models.BooleanField(default=False)
+    is_occupied = models.BooleanField(default=False)
 
     def __str__(self):
         return f" {self.warehouse}"
